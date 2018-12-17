385d59bc1bbc60f99b9dffa6763284535b304b32
xlonywka <45290479+xlonywka@users.noreply.github.com>
2018-12-17 17:06:32 +0300

    Fixed pep8 in translator
    BIG UPDATE 0.1

--- translate.py
+++ translate.py
@@ -1,14 +1,15 @@
 from yandex_translate import YandexTranslate
-translate = YandexTranslate('trnsl.1.1.20181216T165708Z.256f02815004a360.907b12c1d0b00c3d0555378e945df82d09824dd2')
+keylong = '4a360.907b12c1d0b00c3d0555378e945df82d09824dd2'
+translate = YandexTranslate('trnsl.1.1.20181216T165708Z.256f0281500' + keylong)
 
 
 def text_to_text(text, lang_to, lang_from):
-    if lang_to=='English':
-        lang_to_y='en'
-    elif lang_to=='Русский':
-        lang_to_y='ru'
-    if lang_from=='English':
-        lang_from_y='en'
-    if lang_from=='Русский':
-        lang_from_y='ru'
-    return(translate.translate(text, lang_from_y+'-'+lang_to_y).get('text')[0])
+    if lang_to == 'English':
+        lang_to_y = 'en'
+    elif lang_to == 'Русский':
+        lang_to_y = 'ru'
+    if lang_from == 'English':
+        lang_fy = 'en'
+    if lang_from == 'Русский':
+        lang_fy = 'ru'
+    return(translate.translate(text, lang_fy + '-' + lang_to_y).get('text')[0])
