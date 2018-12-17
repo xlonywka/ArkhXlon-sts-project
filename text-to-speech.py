4f2bd9b7652e560a3a11f27e27ee0243d95ccd53
xlonywka <45290479+xlonywka@users.noreply.github.com>
2018-12-17 16:56:56 +0300

    Delete old tts

--- text-to-speech.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-
-
-def speech(text1, lng):
-    tts = gTTS(text=text1, lang=lng)
-    tts.save("speech_on_another_language.mp3")
-    os.system("mpg321 speech_on_another_language.mp3")
